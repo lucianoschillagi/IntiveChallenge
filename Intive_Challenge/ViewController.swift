d11ebd9b5d9fb59fa3e413a0afd0c1787636da05
Luciano Schillagi <luko.web@gmail.com>
2018-09-04 15:21:01 -0300

    second commit

---
+++ Intive_Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Intive_Challenge
+//
+//  Created by Luciano Schillagi on 04/09/2018.
+//  Copyright © 2018 luko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,8 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '10.0'
+use_frameworks!
+
+target 'Intive_Challenge' do
+    pod 'Alamofire', '~> 4.7'
+    pod 'YouTubePlayer-Swift', '~> 1.0'
+end
